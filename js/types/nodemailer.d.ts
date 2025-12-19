e94cca7f60150219ff2799c8fe98663baa36c106
admin <janp2peters@gmail.com>
2025-12-19 15:33:11 +0100

    Fix: Nodemailer

--- js/types/nodemailer.d.ts
+++ js/types/nodemailer.d.ts
@@ -1,1 +1,16 @@
-declare module "nodemailer";
+declare module "nodemailer" {
+  export type Transporter = any;
+  export interface TransportOptions {
+    [key: string]: any;
+  }
+
+  export function createTransport(options?: TransportOptions): Transporter;
+  export const Transporter: Transporter;
+
+  const nodemailer: {
+    createTransport: typeof createTransport;
+    Transporter: typeof Transporter;
+  };
+
+  export default nodemailer;
+}
